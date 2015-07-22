13e7937ddd52f6e548fffdcce766c4a7bf898afa
jarlyyn hsu <jarlyyn@jarlyyn-rmbp>
2015-07-23 00:05:26 +0800

    引入最新lodash,加入isData

--- appTemplate/index.js
+++ appTemplate/index.js
@@ -4,6 +4,8 @@ module.exports=function(webconfig)
   var config=require('./config.js');
   web=new Web(Web.libs.lodash.extend(config,webconfig||{}));
   var app=web.app();
+  // var Settings=web.plugin('seSettings');
+  // web.settings=new Settings('sequelize','mem','_app');
   app.load(__dirname+'/views');
   app.load(__dirname+'/actions');
   app.load(__dirname+'/models');
